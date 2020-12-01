53cd90379f5b7e58eef76ad913bb58b9ffa54222
sandeep.i.32 <sandeep.i.learnweel@gmail.com>
2020-12-02 00:24:40 +0530

    feat: adding queryselector
    adding query selector  and event listener

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('heloo');
+var  btn = document.querySelector('#btn-click');
+var txt = document.querySelector('#txt-area');
+
+// txt.addEventListener('click',click);
+btn.addEventListener('click',click);
+
+function click(){
+    // console.log('clickeddddd');
+    console.log(txt);
+    console.log(txt.value);
+}
