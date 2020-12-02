ee57f0d628c57a641694bce66339a02c5a3aa67d
sandeep.i.32 <sandeep.i.learnweel@gmail.com>
2020-12-02 12:20:22 +0530

    feat:adding api
    adding api minion translato api

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 var  btn = document.querySelector('#btn-click');
 var txt = document.querySelector('#txt-area');
+var output = document.querySelector('#output');
+// var serverUrl = "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" ;
+var serverUrl = "https://api.funtranslations.com/translate/minion.json" ;
 
-// txt.addEventListener('click',click);
-btn.addEventListener('click',click);
+https://api.funtranslations.com/translate/minion.json
 
-function click(){
-    // console.log('clickeddddd');
-    console.log(txt);
-    console.log(txt.value);
+function url(textInput){
+    return  serverUrl + "?" + "text=" + textInput
+
 }
+
+function errorHandler(error){
+        return( "server Error" + error);
+}
+
+
+
+   function process(){
+        var textInput = txt.value ;
+        console.log(textInput);
+        // console.log(url(textInput))
+        fetch(url(textInput))
+        .then(respone => respone.json())
+        .then(
+            json => 
+            output.innerText = (json.contents.translated)
+            )
+        .catch(errorHandler)
+        
+    }
+
+btn.addEventListener('click',process);
+    
+
+
+// .then(json => console.log(json))
+
+ 
+
