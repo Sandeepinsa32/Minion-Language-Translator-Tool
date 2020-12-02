a266d9e38609cc428d940f33a7c95dde0ffaa6bc
sandeep.i.32 <sandeep.i.learnweel@gmail.com>
2020-12-02 22:02:17 +0530

    feat:adding offline indicator

--- app.js
+++ app.js
@@ -1,41 +1,69 @@
 var  btn = document.querySelector('#btn-click');
 var txt = document.querySelector('#txt-area');
 var output = document.querySelector('#output');
-// var serverUrl = "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" ;
-var serverUrl = "https://api.funtranslations.com/translate/minion.json" ;
+var serverUrl = "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" ;
+// var serverUrl = "https://api.funtranslations.com/translate/minion.json" ;
+// var serverUrl = "https://api.funtranslations.com/translate/ferb-latin.json";
 
-https://api.funtranslations.com/translate/minion.json
 
-function url(textInput){
-    return  serverUrl + "?" + "text=" + textInput
-
+function isOnline() { 
+  
+    if (!navigator.onLine) { 
+        document.getElementById("#output").innerText = "Online"; 
+    } else { 
+        document.getElementById("#output").innerText = "Offline"; 
+    } 
 }
 
-function errorHandler(error){
-        return( "server Error" + error);
-}
+
+
+    function url(textInput){
+        return  serverUrl + "?" + "text=" + textInput
+
+    }
+
+    function errorHandler(error){
+            return( "server Error" + error);
+    }
+
 
 
 
    function process(){
-        var textInput = txt.value ;
-        console.log(textInput);
-        // console.log(url(textInput))
-        fetch(url(textInput))
-        .then(respone => respone.json())
-        .then(
-            json => 
-            output.innerText = (json.contents.translated)
-            )
-        .catch(errorHandler)
-        
-    }
+   
+    if(navigator.onLine){   
+                            var textInput = txt.value 
+                            console.log(textInput)
+                    
+                    fetch(url(textInput)).then(respone => respone.json()).then(
+                            json => {
+                                    var outputText =  json.contents.translated ; 
+                                    output.innerText = outputText
+                                    console.log(outputText);
+                                }
+                                )
+                        .catch(errorHandler)  
 
-btn.addEventListener('click',process);
+                        }
+                        else{
+                           var  outputText = "Please!! Connect To Internet.. ,You are offline";
+                                output.innerText = outputText
+                                output.style.color = 'green';
+                        }
+                        // console.log(outputText)
+                       
+                
+                        }
+
+
+
+
+
+    btn.addEventListener('click',process);
     
 
 
-// .then(json => console.log(json))
+
 
  
 
